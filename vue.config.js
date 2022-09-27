f92c6f055e6f15da9931c22aefd4bffa4b2e75eb
chenci20 <2380866751@qq.com>
2022-09-27 21:06:30 +0800

    关闭语法检查并且设置build自启动浏览器

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+
+	// 关闭语法检查
+	lintOnSave: false,
+
+	// 调试配置
+	devServer: {
+		// build自动打开网页
+		open: true,
+		host: 'localhost',
+		port: '8080',
+	},
+});
